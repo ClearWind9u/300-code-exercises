77b7233beba365184ca060f6c60530019144bc76
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:54:32 +0700

    Add files via upload

--- ex47/source.cpp
+++ ex47/source.cpp
@@ -1,1 +1,15 @@
-
+//Tinh tong S voi n cho truoc
+//Neu n chan: S = 2 + 4 + ... + n
+//Neu n le: S = 1 + 3 + ... + n
+#include <iostream>
+using namespace std;
+int main() {
+	int n, sum = 0;
+	cout << "Nhap n: "; cin >> n;
+	if (n % 2) {
+		for (int i = 1; i <= n; i += 2) sum += i;
+	}
+	else for (int i = 2; i <= n; i += 2) sum += i;
+	cout << "S = " << sum << endl;
+	return 0;
+}
