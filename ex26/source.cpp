febeed2d87bb5f0a5b91baa0d715d4ef444c1561
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:40:50 +0700

    Add files via upload

--- ex26/source.cpp
+++ ex26/source.cpp
@@ -1,1 +1,18 @@
-
+//Nhap vao tu so va mau so cua 1 phan so (deu khac 0), rut gon phan so do
+#include <iostream>
+using namespace std;
+int main() {
+	int tu, mau, gcd, dt = 0, dm = 0;
+	string str = "";
+	cout << "Nhap tu so, mau so: "; cin >> tu >> mau;
+	if (tu < 0) { tu = -tu; dt = 1; }
+	if (mau < 0) { mau = -mau; dm = 1; }
+	gcd = tu;
+	while (tu % gcd || mau % gcd) gcd--;
+	tu = tu / gcd; mau = mau / gcd;
+	if (dt + dm == 1) str = "-";
+	cout << "Rut gon: ";
+	if (mau == 1) cout << str << tu;
+	else cout << str << tu << "/" << mau;
+	return 0;
+}
