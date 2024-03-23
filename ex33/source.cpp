1bc9a4e3f172f1074e2901885cea88e4eadfed28
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:46:56 +0700

    Add files via upload

--- ex33/source.cpp
+++ ex33/source.cpp
@@ -1,1 +1,21 @@
-
+//Tim tat ca so Armstrong (tong luy thua cac chu so cua no bang chinh no) co 3, 4 chu so 
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+	int arm, n, sum;
+	cout << "So Armstrong co 3, 4 chu so: \n";
+	for (int i = 100; i < 10000; i++) {
+		if (i < 1000) {
+			n = 3;
+			sum = pow(i / 100, n) + pow(i % 100 / 10, n) + pow(i % 100 % 10, n);
+			if (sum == i) cout << i << " ";
+		}
+		else {
+			n = 4;
+			sum = pow(i / 1000, n) + pow(i % 1000 / 100, n) + pow(i % 1000 % 100 / 10, n) + pow(i % 1000 % 100 % 10, n);
+			if (sum == i) cout << i << " ";
+		}
+	}
+	return 0;
+}
