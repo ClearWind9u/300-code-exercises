7315253fad527eed8cd9296ffabe7ac37f1f04a6
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:36:45 +0700

    Add files via upload

--- ex19/source.cpp
+++ ex19/source.cpp
@@ -1,1 +1,12 @@
-
+//Nhap vao 2 khoang thoi gian. Tinh thoi gian nam giua 2 khoang nay
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+	int hour1, min1, sec1, hour2, min2, sec2, time;
+	cout << "Nhap gio, phut, giay [1]: "; cin >> hour1 >> min1 >> sec1;
+	cout << "Nhap gio, phut, giay [2]: "; cin >> hour2 >> min2 >> sec2;
+	time = abs(3600 * hour1 + 60 * min1 + sec1 - 3600 * hour2 - 60 * min2 - sec2); 
+	cout << "Hieu thoi gian: " << time / 3600 << " gio " << (time % 3600) / 60 << " phut " << (time % 3600) % 60 << " giay " << endl;
+	return 0;
+}
