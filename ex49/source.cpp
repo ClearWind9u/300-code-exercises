9b4ca90fb5a167ebd2694ff19f068bcbe587b43e
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:55:34 +0700

    Add files via upload

--- ex49/source.cpp
+++ ex49/source.cpp
@@ -1,1 +1,18 @@
+//Tinh can so lien tuc
+#include <iostream>
+#include <math.h>
+using namespace std;
 
+double sq(double x, double y) {
+	return pow(y, 1 / x);
+}
+
+int main() {
+	int n; double s = 0;
+	cout << "Nhap n: "; cin >> n;
+	for (int i = 1; i <= n; i++) {
+		s += sq(i + 1, i + s) - s;
+	}
+	cout << "Ket qua: " << s;
+	return 0;
+}
