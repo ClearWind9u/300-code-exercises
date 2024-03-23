c0dfe1312f9a8904f7fb9a53820bc9393be9b2e4
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:17:52 +0700

    Add files via upload

--- ex6/source.cpp
+++ ex6/source.cpp
@@ -1,1 +1,26 @@
+//In ra 3 so theo thu tu tang dan, chi dung 1 bien phu
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b, c, tmp;
+	cout << "Nhap a, b, c: ";
+	cin >> a >> b >> c;
+	tmp = a;
+	if (tmp > b) tmp = b;
+	if (tmp > c) tmp = c;
+	cout << "Tang dan: " << tmp;
+	if (tmp == a) {
+		if (b > c) cout << " " << c << " " << b;
+		else cout << " " << b << " " << c;
+	}
+	else if (tmp == b) {
+		if (a > c) cout << " " << c << " " << a;
+		else cout << " " << a << " " << c;
+	}
+	else {
+		if (a > b) cout << " " << b << " " << a;
+		else cout << " " << a << " " << b;
+	}
 
+	return 0;
+}
