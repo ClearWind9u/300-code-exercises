3bf7a4e1b6d5b55c21ec27f03b5998c1a312f686
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:38:31 +0700

    Add files via upload

--- ex22/source.cpp
+++ ex22/source.cpp
@@ -1,1 +1,18 @@
-
+//Liet ke, dem va tinh tong cac uoc so cua so nguyen duong n
+#include <iostream>
+using namespace std;
+int main() {
+	int n, count = 0, sum = 0;
+	cout << "Nhap n: "; cin >> n;
+	cout << "Cac uoc so: ";
+	for (int i = 1; i <= n; i++) {
+		if (n % i == 0) {
+			cout << i << " ";
+			count++;
+			sum += i;
+		}
+		if (i == n) cout << endl;
+	}
+	cout << "Co " << count << " uoc so, tong la: " << sum << endl;
+	return 0;
+}
