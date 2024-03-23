8c5d4a738b8aa56ffd3ec04fb4110f012532be05
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:14:02 +0700

    Add files via upload

--- ex3/source.cpp
+++ ex3/source.cpp
@@ -1,1 +1,18 @@
-
+//Xac dinh vi tri tuong doi cua diem M voi duong tron C
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+	double xC, yC, r, xM, yM, d;
+	cout << "Nhap toa do tam C(xC, yC)? ";
+	cin >> xC >> yC;
+	cout << "Nhap ban kinh R? ";
+	cin >> r;
+	cout << "Nhap toa do M(xM, yM)? ";
+	cin >> xM >> yM;
+	d = sqrt((xC - xM) * (xC - xM) + (yC - yM) * (yC - yM));
+	if (d > r)	cout << "M nam ngoai C()";
+	else if (d == r) cout << "M nam tren C()";
+	else cout << "M nam trong C()";
+	return 0;
+}
