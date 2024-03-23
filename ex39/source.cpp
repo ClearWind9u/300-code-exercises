61e793b09f5c806e472760581022e33463662b3a
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:50:08 +0700

    Add files via upload

--- ex39/source.cpp
+++ ex39/source.cpp
@@ -1,1 +1,14 @@
-
+//Tim bo ba pythagorean nho hon 100 la 3 so nguyen lien tiep hoac 3 so chan lien tiep
+#include <iostream>
+using namespace std;
+int main() {
+	for (int i = 1; i < 100; i++) {
+		if (i * i + (i + 1) * (i + 1) == (i + 2) * (i + 2) && i < 98) {
+			cout << "( " << i << ", " << i + 1 << ", " << i + 2 << " ) : ba so nguyen lien tiep\n";
+		}
+		if (i * i + (i + 2) * (i + 2) == (i + 4) * (i + 4) && i < 96) {
+			cout << "( " << i << ", " << i + 2 << ", " << i + 4 << " ) : ba so chan lien tiep\n";
+		}
+	}
+	return 0;
+}
