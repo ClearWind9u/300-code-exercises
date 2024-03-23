1c32df697cfbf02b83821bca8a37ee9bbbac6a1b
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:46:13 +0700

    Add files via upload

--- ex32/source.cpp
+++ ex32/source.cpp
@@ -1,1 +1,21 @@
-
+//Viet chuoi Hailstones sinh ra voi n nhap vao tu ban phim
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+	string str;
+	label:
+	int n, count = 0;
+	cout << "Nhap n: "; cin >> n;
+	cout << setw(5) << n; count++;
+	while (n != 1) {
+		if (n % 2 == 0) n = n / 2;
+		else n = 3 * n + 1;
+		cout << setw(5) << n; count++;
+		if (count % 6 == 0) cout << endl;
+	}
+	cout << endl << "Hailstones sinh duoc: " << count << endl;
+	cout << "Tiep (y/n)? "; cin >> str;
+	if (str == "y") goto label;
+	else return 0;
+}
