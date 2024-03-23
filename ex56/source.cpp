0b301eebe536de179c02212adc2a9baeef0ac81a
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:59:18 +0700

    Add files via upload

--- ex56/source.cpp
+++ ex56/source.cpp
@@ -1,1 +1,20 @@
+//Nhap vao 1 so nguyen n co dau, in ra dang nhi phan va thap luc phan cua n
+#include <iostream>
+using namespace std;
 
+void printBits(int x) {
+	int size = sizeof(int) * 8;
+	cout << x << " = ";
+	while (--size >= 0) {
+		putchar(((x >> size) & 1) + '0');
+		if (size % 8 == 0) putchar(' ');
+	}
+	putchar('\n');
+}
+int main() {
+	int n;
+	cout << "Nhap n: "; cin >> n;
+	printBits(n);
+	cout << "Hex: " << uppercase << hex << n;
+	return 0;
+}
