d4127008296a70509f9b20d4c34fec289ef9026b
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:12:38 +0700

    Add files via upload

--- ex1/source.cpp
+++ ex1/source.cpp
@@ -1,1 +1,14 @@
-
+// Nhap vao dien tich S cua mat cau. Tinh the tich V cua mat cau do
+#include <iostream>
+#include <numbers>
+using namespace std;
+int main() {
+	double S, V, r;
+	const double PI = atan(1) * 4;
+	cout << "Nhap dien tich S: ";
+	cin >> S;
+	r = sqrt(S*1.0/(4*PI));
+	V = 4.0 / 3 * PI * r * r * r;
+	cout << "The tich V = " << V;
+	return 0;
+}
