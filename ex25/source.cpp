d96cae0402b21e4ce2cf77fbc99c01a0d6e39ed0
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:40:09 +0700

    Add files via upload

--- ex25/source.cpp
+++ ex25/source.cpp
@@ -1,1 +1,14 @@
-
+//Tinh UCLN va BCNN cho 2 so a, b
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b, gcd, lcm;
+	cout << "Nhap cap (a, b): "; cin >> a >> b;
+	gcd = a;
+	while (a % gcd || b % gcd) gcd--;
+	lcm = a;
+	while (lcm % a || lcm % b) lcm++;
+	cout << "USCLN (a, b): " << gcd << endl;
+	cout << "BSCNN (a, b): " << lcm << endl;
+	return 0;
+}
