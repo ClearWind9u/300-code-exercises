b187ee83d82538b2b0123db2bed30cbf35817e99
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:45:38 +0700

    Add files via upload

--- ex31/source.cpp
+++ ex31/source.cpp
@@ -1,1 +1,13 @@
-
+//In bang cuu chuong tu 2 den 9
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+	for (int i = 1; i <= 10; i++) {
+		for (int j = 2; j <= 9; j++) {
+			cout << "|" << j << "x" << setw(2) << i << "= " << setw(2) << j * i;
+		}
+		cout << "|" << endl;
+	}
+	return 0;
+}
