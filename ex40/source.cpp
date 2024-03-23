0786fc8d96451fed7609db29357d93dedf34078f
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:50:42 +0700

    Add files via upload

--- ex40/source.cpp
+++ ex40/source.cpp
@@ -1,1 +1,11 @@
-
+//Tim trau dung, trau nam, trau gia thoa man bai toan co
+#include <iostream>
+using namespace std;
+void main() {
+	for (int d = 1; d < 20; d++) {
+		for (int n = 1; n < 33; n++) {
+			int g = (100 - d * 5 - n * 3) * 3;
+			if (d + n + g == 100) cout << "(" << d << ", " << n << ", " << g << ")\n";
+		}
+	}
+}
