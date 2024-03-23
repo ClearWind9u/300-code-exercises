75fec3a2a095cba0673cea3217d4ca159e6596d1
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:41:26 +0700

    Add files via upload

--- ex27/source.cpp
+++ ex27/source.cpp
@@ -1,1 +1,22 @@
-
+//Phan tich so nguyen duong n thanh cac thua so nguyen to
+#include <iostream>
+using namespace std;
+bool check(int n) {
+	for (int i = 2; i < n; i++) {
+		if (n % i == 0) return false;
+	}
+	return true;
+}
+int main() {
+	int n, i = 2;
+	cout << "Nhap n: "; cin >> n;
+	while (i != n) {
+		if (check(i)) {
+			if ((n % i) == 0) { cout << i << " * "; n = n / i; }
+			else i++;
+		}
+		else i++;
+	}
+	cout << n;
+	return 0;
+}
