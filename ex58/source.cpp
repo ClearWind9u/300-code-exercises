b99d37bcc06289f3356f692d49d2bf00a36b2f27
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 18:00:24 +0700

    Add files via upload

--- ex58/source.cpp
+++ ex58/source.cpp
@@ -1,1 +1,15 @@
-
+//Thuc hien thuat toan sang Erastosthenes in ra cac so nguyen to nho hon n so cho truoc (n < 100)
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	label:
+	cout << "Nhap n: "; cin >> n;
+	if (n >= 100) { cout << "Error! (n < 100). "; goto label; }
+	int ar[100] = { 0 };
+	for (int i = 2; i <= n; i++) {
+		if (!ar[i]) cout << i << " ";
+		for (int j = i + i; j <= n; j += i)	ar[j] = 1;
+	}
+	return 0;
+}
