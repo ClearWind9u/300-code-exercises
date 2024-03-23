624af68bc5a15387ef306d40d87e9b5df7861876
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:34:40 +0700

    Add files via upload

--- ex15/source.cpp
+++ ex15/source.cpp
@@ -1,1 +1,15 @@
-
+//Nhap vao ngay, thang, nam (gia su nhap dung). Tinh xem do la ngay thu bao nhieu trong nam.
+#include <iostream>
+using namespace std;
+int main() {
+	unsigned int sum, day, month, year;
+	bool nhuan;
+	cout << "Nhap ngay, thang, nam: "; cin >> day >> month >> year;
+	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) nhuan = 1;
+	else nhuan = 0;
+	sum = (int)(30.42 * (month - 1)) + day;
+	if (month == 2 || (month > 2 && nhuan)) sum++;
+	if (month > 2 && month < 8) sum--;
+	cout << "Ngay thu: " << sum << endl;
+	return 0;
+}
