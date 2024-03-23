69f6cab1603802df11f206b5e0e06339d752f6f4
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:19:04 +0700

    Add files via upload

--- ex7/source.cpp
+++ ex7/source.cpp
@@ -1,1 +1,12 @@
-
+// Giai pt bac 1: ax + b = 0
+#include <iostream>
+using namespace std;
+int main() {
+	double a, b, x;
+	cout << "Nhap a, b: "; cin >> a >> b;
+	if (!a) { b ? cout << "Phuong trinh vo nghiem" : cout << "Phuong trinh vo so nghiem"; return 0; }
+	if (!b) { cout << "x = " << 0; return 0; }
+	x = -b / a;
+	cout << "x = " << x;
+	return 0;
+}
