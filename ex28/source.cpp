247c08aa817a6e7cd29fa67df7ac9bea9631f30d
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:42:52 +0700

    Add files via upload

--- ex28/source.cpp
+++ ex28/source.cpp
@@ -1,1 +1,18 @@
-
+//Mo phong ham ROUND cua Excel, dung lam trong 1 so double voi 1 so n cho truoc
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+int main() {
+	double x; int n; long mu;
+	cout << "Nhap so thuc x: "; cin >> x;
+	cout << "Do chinh xac: "; cin >> n;
+	if (n >= 0) {
+		cout << setprecision(n) << fixed << x;
+	}
+	else {
+		mu = pow(10, -n);
+		cout << round(x / mu) * mu;
+	}
+	return 0;
+}
