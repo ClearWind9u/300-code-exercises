6f86dacd665af4433f3c703d517401ee95a3586e
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:32:27 +0700

    Add files via upload

--- ex11/source.cpp
+++ ex11/source.cpp
@@ -1,1 +1,21 @@
-
+//Viet tro choi bao - da - keo
+#include <iostream>
+using namespace std;
+int main() {
+	char player, computer; int random, pp = 0, cp = 0;
+	srand(time(NULL));
+	while (1) {
+		cout << "Nhap ky tu (b-d-k), ky tu khac de thoat: "; cin >> player;
+		if (player != 'b' && player != 'd' && player != 'k') break;
+		random = rand() % 3;
+		if (random == 0) computer = 'b';
+		if (random == 1) computer = 'd';
+		if (random == 2) computer = 'k';
+		cout << "Computer: " << computer << endl;
+		if (player == 'b') { if (computer == 'k') cp++; if (computer == 'd') pp++; }
+		else if (player == 'd') { if (computer == 'b') cp++; if (computer == 'k') pp++; }
+		else { if (computer == 'd') cp++; if (computer == 'b') pp++; }
+		cout << "Ty so human - computer: " << pp << " - " << cp << endl;
+	}
+	return 0;
+}
