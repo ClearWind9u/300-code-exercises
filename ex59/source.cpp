c4817c244af738a8b66f3e6e0583609e2cb06c97
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 18:00:53 +0700

    Add files via upload

--- ex59/source.cpp
+++ ex59/source.cpp
@@ -1,1 +1,13 @@
-
+//Nhap vao nam Duong lich, xuat ra nam Am lich tuong ung va nam Duong lich ke tiep co cung nam Am lich
+#include <iostream>
+using namespace std;
+int main() {
+	int nam, a;
+	string can[10] = {"Canh", "Tan", "Nham", "Quy", "Giap", "At", "Binh", "Dinh", "Mau", "Ky"};
+	string chi[12] = { "Than", "Dau", "Tuat", "Hoi", "Ty", "Suu", "Dan", "Mao", "Thin", "Ty", "Ngo", "Mui" };
+	cout << "Nhap nam: "; cin >> nam;
+	a = nam % 60 % 12;
+	cout << nam << " - " << can[(a + (nam % 60 / 12) * 2) % 10] << " " << chi[a] << endl;
+	cout << nam + 60 << " - " << can[(a + (nam % 60 / 12) * 2) % 10] << " " << chi[a] << endl;
+	return 0;
+}
