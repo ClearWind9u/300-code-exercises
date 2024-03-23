57d958aff3eec9c132b53c43f74adb4d83815f3f
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:36:10 +0700

    Add files via upload

--- ex18/source.cpp
+++ ex18/source.cpp
@@ -1,1 +1,13 @@
-
+//Nhap vao so gio. Doi sang so tuan, ngay, gio tuong ung
+#include <iostream>
+using namespace std;
+int main() {
+	unsigned int hours, week, day, hour;
+	cout << "Nhap so gio: "; cin >> hours;
+	week = hours / (24 * 7);
+	hours = hours % (24 * 7);
+	day = hours / 24;
+	hour = hours % 24;
+	cout << week << " tuan, " << day << " ngay, " << hour << " gio" << endl;
+	return 0;
+}
