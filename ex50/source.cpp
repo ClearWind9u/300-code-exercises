6ec07c46ecc374779c01eb0b86ab3cbdc5ba4169
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:56:08 +0700

    Add files via upload

--- ex50/source.cpp
+++ ex50/source.cpp
@@ -1,1 +1,26 @@
-
+// Cho s/t = 1/(b1 + 1/(b2 + 1/...(bk-1 + 1/bk)))
+//Cho s va t, tim b1, b2... bk
+#include <iostream>
+using namespace std;
+int main() {
+	int s, t, a, r;
+	label:
+	cout << "Nhap s, t (0 < s < t): "; cin >> s >> t;
+	if (s <= 0 || s >= t) { cout << "Error! (0 < s < t). "; goto label; }
+	a = t / s; r = t % s;
+	cout << "[" << a;
+	if (r != 0) {
+		a = s / r; s = s % r;
+		cout << ", " << a;
+		for (int i = 1; r != 0 && s != 0; cout << ", " << a, i++) {
+			if (i % 2) {
+				a = r / s; r = r % s;
+			}
+			else {
+				a = s / r; s = s % r;
+			}
+		}
+	}
+	cout << "]\n";
+	return 0;
+}
