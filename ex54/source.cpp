aea70d3a909033d44318f50dba7112c7ff961b22
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:58:18 +0700

    Add files via upload

--- ex54/source.cpp
+++ ex54/source.cpp
@@ -1,1 +1,20 @@
+//Dung vong lap, tinh to hop chap k cua n (k < n < 25)
+#include <iostream>
+using namespace std;
 
+int combination(int k, int n) {
+	int kq = 1;
+	for (int i = 1; i <= n - k; i++) {
+		kq *= n - i + 1; kq /= i;
+	}
+	return kq;
+}
+
+int main() {
+	int k, n;
+	label:
+	cout << "Nhap n, k (k < n < 25): "; cin >> n >> k;
+	if (n <= k || n >= 25 || k <= 0) { cout << "Error! (k < n < 25). "; goto label; }
+	cout << "C ( k, n ): " << combination(k, n) << endl;
+	cout << "C ( n-k, n ): " << combination(n - k, n) << endl;
+}
