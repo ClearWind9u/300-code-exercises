0758131f144a009a00c6202ad30bc78e811adcc7
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:59:51 +0700

    Add files via upload

--- ex57/source.cpp
+++ ex57/source.cpp
@@ -1,1 +1,15 @@
-
+//Xac dinh bit parity chan cua n
+#include <iostream>
+using namespace std;
+int main() {
+	int n, count = 0, size;
+	cout << "Nhap n: "; cin >> n;
+	size = sizeof(int) * 8;
+	while (--size >= 0) {
+		if ((n >> size) & 1) count++;
+	}
+	cout << "Even parity bit = ";
+	if (count % 2 == 0) cout << 0;
+	else cout << 1;
+	return 0;
+}
