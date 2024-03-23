8de21ca7eda970418501e58366de818989d01c41
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:53:40 +0700

    Add files via upload

--- ex46/source.cpp
+++ ex46/source.cpp
@@ -1,1 +1,16 @@
-
+//Kiem tra 2 ve cua cong thuc voi n bat ki
+// xich ma i chay tu 1 den n (i^3) = n^2.(n + 1)^2/4
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+	int vp = 0, vt = 0, n;
+	cout << "Nhap n: "; cin >> n;
+	for (int i = 1; i <= n; i++) {
+		vt += pow(i, 3);
+	}
+	vp = pow(n, 2) * pow(n + 1, 2) / 4;
+	cout << "Ve trai = " << vt << endl;
+	cout << "Ve phai = " << vp << endl;
+	return 0;
+}
