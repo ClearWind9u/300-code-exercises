ee10d504ecc3eba8d5c770a0a36917ce20b31e53
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:13:17 +0700

    Add files via upload

--- ex2/source.cpp
+++ ex2/source.cpp
@@ -1,1 +1,14 @@
-
+//Tinh khoang cach 2 diem A, B
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+	double xA, yA, xB, yB, d;
+	cout << "A(xA, yA)? ";
+	cin >> xA >> yA;
+	cout << "B(xB, yB)? ";
+	cin >> xB >> yB;
+	d = sqrt((xA - xB) * (xA - xB) + (yA - yB) * (yA - yB));
+	cout << "|AB| = " << d;
+	return 0;
+}
