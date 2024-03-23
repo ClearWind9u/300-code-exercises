42f0f899a4f20c68fa54498b89ae11b17fbaea10
Phạm Thanh Phong <133841678+ClearWind9u@users.noreply.github.com>
2024-03-23 17:48:27 +0700

    Add files via upload

--- ex36/source.cpp
+++ ex36/source.cpp
@@ -1,1 +1,18 @@
-
+//In ra n so nguyen to dau tien
+#include <iostream>
+using namespace std;
+bool check(int x) {
+	for (int i = 2; i < x; i++) {
+		if (x % i == 0) return false;
+	}
+	return true;
+}
+int main() {
+	int n, count = 0;
+	cout << "Nhap n: "; cin >> n;
+	for (int i = 2; ; i++) {
+		if (count == n) break;
+		if (check(i)) { cout << i << " "; count++; }
+	}
+	return 0;
+}
